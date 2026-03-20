d8ebd22cffa821c5154ad7eb23c0d63cd62f05ab
deepakdotdevs <deepak.06934@gmail.com>
2026-03-20 21:25:09 +0530

    Data Types in Kotlin #Comments

---
+++ Data Types/datatype.kt
@@ -0,0 +1,9 @@
+// Mostly Used Data Types : 
+
+Int (Integer) -> 4 bytes -> eg; 10, 20, 30
+Long (Integer)-> 8 bytes -> eg; 10000000000L
+Float (Floating Point)-> 4 bytes -> eg; 10.5F, 147.85f
+Double (Floating Point) -> 8 bytes -> eg; 10478542.5, 1447854267.85, 12.4, 10.1
+Char (Character) -> 2 bytes -> eg; 'A', 'B', 'C'
+String (Word / Sentences)-> 2 bytes per character -> eg; "Hello", "Kotlin", "Data Types", "123456789"
+Boolean (True / False) -> 1 byte -> eg; true, false
--- hlo.kt
+++ hlo.kt
@@ -1,3 +1,6 @@
+// Enry point of the kotlin application is the main function, this is the initial point
+// which is execute first or we say it is the mouth of the file or of the program.
+
 // Method No. 1 (Without args (arguments or parameters))
 
 // fun main() {
@@ -9,3 +12,14 @@
 fun main(args: Array<String>) {
     println("Hello World !!")
 }
+
+// Comments in Kotlin : 
+
+// This is the Single line Comment
+
+/* 
+hlo
+hloo
+hlooo
+This is the multiple line comment 
+*/
