035a925c245c47bd7a599116e487b22548dc7217
deepakdotdevs <deepak.06934@gmail.com>
2026-03-20 21:54:30 +0530

    var variable & print, println

---
+++ Data Types/variable.kt
@@ -0,0 +1,29 @@
+// Variable -> named a container used to store data values in a computer's memory
+// Two types of variables in kotlin -> var and val
+
+// var -> Variables that can be reassigned, it is dynamic type
+
+fun main(){
+    var roll = 10; // roll is variable name and 10 (integer) value is data which is stored
+    roll = 20; // reassigning the value
+
+    var name = "Kotlin" // String type
+    var mobile_no = 9518800000L // Long type
+    var price = 142.82f // Float type
+    var totalCost = 147852369.452 // Double type
+    var isActive = false; // Boolean type
+    var gender = 'M' // Char type
+    var address = "123 Main St" // String type
+
+    // print() -> print next values in one line
+    // println() -> print next values in new line
+
+    println(roll)
+    println(name)
+    println(mobile_no)
+    println(price)
+    println(totalCost)
+    println(isActive)
+    println(gender)
+    print(address)
+}
