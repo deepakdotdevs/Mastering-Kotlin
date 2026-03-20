3a7e75a74cb9b2733679c6f79a6a9e8adb8bae1d
deepakdotdevs <deepak.06934@gmail.com>
2026-03-20 22:15:00 +0530

    var & val & it's Dynamic and Specific data types

--- Data Types/specificdatatype.kt
+++ Data Types/specificdatatype.kt
@@ -1,12 +1,55 @@
+// Specific Type of var : 
+
+// fun main() {
+//     var roll: Int = 10
+//     var name: String = "Kotlin"
+//     var mobile_no: Long = 9518800000L
+//     var price: Float = 142.82f
+//     var totalCost: Double = 147852369.452
+//     var isActive: Boolean = false
+//     var gender: Char = 'M'
+//     var address: String = "123 Main St"
+
+//     println(roll)
+//     println(name)
+//     println(mobile_no)
+//     println(price)
+//     println(totalCost)
+//     println(isActive)
+//     println(gender)
+//     println(address)
+
+//     // reassign
+//     roll = 20
+//     name = "Kotlin Updated"
+//     mobile_no = 1234567890L
+//     price = 150.0f
+//     totalCost = 150000000.0
+//     isActive = true
+//     gender = 'F'
+//     address = "456 Main St"
+
+//     println(roll)
+//     println(name)
+//     println(mobile_no)
+//     println(price)
+//     println(totalCost)
+//     println(isActive)
+//     println(gender)
+//     println(address)
+// }
+
+// Specific Type of val :
+
 fun main() {
-    var roll: Int = 10
-    var name: String = "Kotlin"
-    var mobile_no: Long = 9518800000L
-    var price: Float = 142.82f
-    var totalCost: Double = 147852369.452
-    var isActive: Boolean = false
-    var gender: Char = 'M'
-    var address: String = "123 Main St"
+    val roll: Int = 10
+    val name: String = "Kotlin"
+    val mobile_no: Long = 9518800000L
+    val price: Float = 142.82f
+    val totalCost: Double = 147852369.452
+    val isActive: Boolean = false
+    val gender: Char = 'M'
+    val address: String = "123 Main St"
 
     println(roll)
     println(name)
@@ -18,21 +61,21 @@ fun main() {
     println(address)
 
     // reassign
-    roll = 20
-    name = "Kotlin Updated"
-    mobile_no = 1234567890L
-    price = 150.0f
-    totalCost = 150000000.0
-    isActive = true
-    gender = 'F'
-    address = "456 Main St"
+    // roll = 20
+    // name = "Kotlin Updated"
+    // mobile_no = 1234567890L
+    // price = 150.0f
+    // totalCost = 150000000.0
+    // isActive = true
+    // gender = 'F'
+    // address = "456 Main St"
 
-    println(roll)
-    println(name)
-    println(mobile_no)
-    println(price)
-    println(totalCost)
-    println(isActive)
-    println(gender)
-    println(address)
+    // println(roll)
+    // println(name)
+    // println(mobile_no)
+    // println(price)
+    // println(totalCost)
+    // println(isActive)
+    // println(gender)
+    // println(address)
 }
--- Data Types/variable.kt
+++ Data Types/variable.kt
@@ -1,7 +1,9 @@
 // Variable -> named a container used to store data values in a computer's memory
 // Two types of variables in kotlin -> var and val
 
-// var -> Variables that can be reassigned, it is dynamic type
+// var -> Variables that can be reassigned ->Dynamic type and Specific Type
+
+// This is Dynamic Type of var : 
 
 // fun main(){
 //     var roll = 10; // roll is variable name and 10 (integer) value is data which is stored
@@ -32,3 +34,45 @@
 //     println(gender)
 
 // }
+
+// val -> Variables that cannot be reassigned -> Dynamic Type and Specific Type
+// This is Dynamic Type of val :
+
+fun main() {
+    val roll= 10
+    val name = "Kotlin"
+    val mobile_no= 9518800000L
+    val price= 142.82f
+    val totalCost = 147852369.452
+    val isActive = false
+    val gender = 'M'
+    val address= "123 Main St"
+
+    println(roll)
+    println(name)
+    println(mobile_no)
+    println(price)
+    println(totalCost)
+    println(isActive)
+    println(gender)
+    println(address)
+
+    // reassign
+    // roll = 20
+    // name = "Kotlin Updated"
+    // mobile_no = 1234567890L
+    // price = 150.0f
+    // totalCost = 150000000.0
+    // isActive = true
+    // gender = 'F'
+    // address = "456 Main St"
+
+    // println(roll)
+    // println(name)
+    // println(mobile_no)
+    // println(price)
+    // println(totalCost)
+    // println(isActive)
+    // println(gender)
+    // println(address)
+}
