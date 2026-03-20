728a94367426696cfd5f815c2e2b04757ee66625
deepakdotdevs <deepak.06934@gmail.com>
2026-03-20 22:05:33 +0530

    specific data type

---
+++ Data Types/specificdatatype.kt
@@ -0,0 +1,38 @@
+fun main() {
+    var roll: Int = 10
+    var name: String = "Kotlin"
+    var mobile_no: Long = 9518800000L
+    var price: Float = 142.82f
+    var totalCost: Double = 147852369.452
+    var isActive: Boolean = false
+    var gender: Char = 'M'
+    var address: String = "123 Main St"
+
+    println(roll)
+    println(name)
+    println(mobile_no)
+    println(price)
+    println(totalCost)
+    println(isActive)
+    println(gender)
+    println(address)
+
+    // reassign
+    roll = 20
+    name = "Kotlin Updated"
+    mobile_no = 1234567890L
+    price = 150.0f
+    totalCost = 150000000.0
+    isActive = true
+    gender = 'F'
+    address = "456 Main St"
+
+    println(roll)
+    println(name)
+    println(mobile_no)
+    println(price)
+    println(totalCost)
+    println(isActive)
+    println(gender)
+    println(address)
+}
