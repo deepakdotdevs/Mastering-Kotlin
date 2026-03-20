3a5a44e15cacf9720b396ab373c29d1d73ff0567
deepakdotdevs <deepak.06934@gmail.com>
2026-03-20 21:10:04 +0530

    Hlo Kotlin #2 Methods (with and without args)

--- hlo.kt
+++ hlo.kt
@@ -1,3 +1,11 @@
-fun main() {
-    println("Hello, Kotlin")
+// Method No. 1 (Without args (arguments or parameters))
+
+// fun main() {
+//     println("Hello, Kotlin")
+// }
+
+// Method No. 2 (With args -> argument)
+
+fun main(args: Array<String>) {
+    println("Hello World !!")
 }
