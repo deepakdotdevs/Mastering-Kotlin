db724241e2b49c98e0e04d2dba92b76bcf83c3e2
deepakdotdevs <deepak.06934@gmail.com>
2026-03-21 12:29:36 +0530

    Arthimetical Operators -> Add, Sub, Multiply, Divide, Remainder

---
+++ Arthimetical Operators/arthimetic.kt
@@ -0,0 +1,13 @@
+//  Arthimetic Operations -> Addition, Subtraction, Multiplication, Division etc
+
+fun main(args: Array<String>) {
+    val a = 10
+    val b = 20
+
+    println("Addition of $a and $b  = ${a+b}")
+    println("Subtraction of $a and $b  = ${a-b}")
+    println("Multiplication of $a and $b  = ${a*b}")
+    println("Division of $a and $b  = ${a/b}")
+    println("Remainder of $a and $b  = ${a%b}")
+    println("----------------------------------")
+}
