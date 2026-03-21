7cc0fe864a2ad8534d4fbb81793e5fbef69240c9
deepakdotdevs <deepak.06934@gmail.com>
2026-03-21 13:19:55 +0530

    Comparison Operators

--- Arthimetical Operators/arthimetic.kt
+++ Arthimetical Operators/arthimetic.kt
@@ -2,12 +2,19 @@
 
 fun main(args: Array<String>) {
     val a = 10
-    val b = 20
+    val b = 2
 
     println("Addition of $a and $b  = ${a+b}")
     println("Subtraction of $a and $b  = ${a-b}")
     println("Multiplication of $a and $b  = ${a*b}")
     println("Division of $a and $b  = ${a/b}")
-    println("Remainder of $a and $b  = ${a%b}")
+    println("Remainder of $a and $b  = ${a%b}") // % -> mod symbol for remainder
     println("----------------------------------")
+
+    // Also there is a Operator : 
+    println(b..a) // it will use in the for loop, to print all of the nos between them
+    // Example : 
+    for(i in b..a) {
+        println(i)
+    }
 }
---
+++ Arthimetical Operators/comparision.kt
@@ -0,0 +1,13 @@
+// Comparison Operations ->  Greater than, Less than, Greather than equal to, Less than equal to
+// Output is Boolean 
+
+fun main() {
+     val a  = 10;
+     val b = 20;
+     val c = 30;
+
+     println(a>b)
+     println(a<b);
+     println(a<=b)
+     println(a>=b)
+}
