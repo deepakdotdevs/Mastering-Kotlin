3b32fe387c630d1f292bc620b9479bf6adb052d6
deepakdotdevs <deepak.06934@gmail.com>
2026-03-21 14:08:56 +0530

    Augmented Assignments

---
+++ Arthimetical Operators/augmented.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var a = 10
+    a += 5 // means 15
+    println(a) // return 15
+    a -= 5
+    println(a) // return 10
+    a *= 2
+    println(a) // return 20
+    a /= 2 
+    println(a) // return 10
+    a %= 5
+    println(a) // return 0
+}
