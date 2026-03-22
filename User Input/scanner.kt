a3ff85a67edea9a387b0c09622e804f1957e431f
deepakdotdevs <deepak.06934@gmail.com>
2026-03-22 12:48:01 +0530

    User Input using Scanner class for Integer

--- User Input/scanner.kt
+++ User Input/scanner.kt
@@ -9,4 +9,10 @@ fun main() {
     println("Your name is : $name")
     println("Data Type of Name : ${name::class.simpleName}") 
 
+    // For Integer : 
+    println("Enter 2 Digits = ")
+    val digits = scanner.nextInt()
+    println("These two digits are : $digits")
+    println("Data Type of Digits : ${digits::class.simpleName}")
+
 }
