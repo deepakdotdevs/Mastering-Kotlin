eb66fb6dfafab42e8756b0dea490e2082ad65009
deepakdotdevs <deepak.06934@gmail.com>
2026-03-22 22:11:26 +0530

    String Concatenation

---
+++ String/string_Concatenation.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val str1 = "Hello"
+    val str2 = " Kotlin"
+    println(str1 + str2)
+    println("This is "+ str1 + str2)
+    println("100" + 50) // output is 10050, it will automatically convert 50 into string
+    // println(50+"100") // provides error 
+}
