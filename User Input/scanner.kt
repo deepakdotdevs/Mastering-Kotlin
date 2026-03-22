3a1a788f8809305498f99adcc22934b136abb07b
deepakdotdevs <deepak.06934@gmail.com>
2026-03-22 12:45:31 +0530

    User Input Using Scanner class for #String

--- User Input/scanner.kt
+++ User Input/scanner.kt
@@ -1,5 +1,12 @@
 // Take User Input using Scanner class : 
-
+import java.util.Scanner
 fun main() {
-    
+    val scanner = Scanner(System.`in`)
+
+    // For String : 
+    println("Enter Your Name = ")
+    val name = scanner.next()
+    println("Your name is : $name")
+    println("Data Type of Name : ${name::class.simpleName}") 
+
 }
