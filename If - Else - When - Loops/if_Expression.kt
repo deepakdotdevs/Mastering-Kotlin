cc31880bbf6a4d393f83d23a1ccdc4fdf1e04d1e
deepakdotdevs <deepak.06934@gmail.com>
2026-03-23 21:53:36 +0530

    if Expression Part II

--- If - Else - When - Loops/if_Expression.kt
+++ If - Else - When - Loops/if_Expression.kt
@@ -9,3 +9,18 @@
 //         println("10 is Greater than 5")
 //     }
 // }
+
+// ii)
+
+fun main() {
+    val a = 20;
+    val b = 10
+    var max = 0; // because it's value is will be assign in next steps
+    if(a>b) max = a
+    println("Then, Updated Max Value is = $max")
+
+    if(b>a) { // not execute, coz b is less than a (b<a)
+        println("B is Max value = $b")
+    }
+    
+}
