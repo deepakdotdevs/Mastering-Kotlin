8fcf54cca3f589e7c2954cfca129bb32dc8ed829
deepakdotdevs <deepak.06934@gmail.com>
2026-03-23 21:44:45 +0530

    if Expression Part - 1

---
+++ If - Else - When - Loops/if_Expression.kt
@@ -0,0 +1,11 @@
+// i)
+
+// fun main() {
+//     // if(10>5) { // return or output is shown, coz condition is true
+//     //     println("10 is Greater than 5")
+//     // }
+
+//     if(10<5) { // Give no output, coz condition is not true
+//         println("10 is Greater than 5")
+//     }
+// }
