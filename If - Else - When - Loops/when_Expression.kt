91b5629f5e34320208c6798fc495ffc3908cba41
deepakdotdevs <deepak.06934@gmail.com>
2026-03-24 21:26:21 +0530

    When Expression -> II)

--- If - Else - When - Loops/when_Expression.kt
+++ If - Else - When - Loops/when_Expression.kt
@@ -1,11 +1,23 @@
-// i) 
+// // i) 
+
+// fun main() {
+//     val x =3 // when x is except 1,2,3 then else is work 
+//     when(x){
+//         1 -> println("One")
+//         2 -> println("Two")
+//         3 -> println("Four")
+//         else -> println("Not Valid")
+//     }
+// }
+
+// ii)
 
 fun main() {
-    val x =3 // when x is except 1,2,3 then else is work 
-    when(x){
-        1 -> println("One")
-        2 -> println("Two")
-        3 -> println("Four")
-        else -> println("Not Valid")
+    val x = 1;
+    when(x) {
+        1,2 -> println("One or Two")
+        3 -> println("Three")
+        4 -> println("Four")
+        else -> println("not valid")
     }
 }
