19462a755897265060d1258b8380a838aa475d53
deepakdotdevs <deepak.06934@gmail.com>
2026-03-25 22:51:47 +0530

    While Loop (true)

--- If - Else - When - Loops/while_Loop.kt
+++ If - Else - When - Loops/while_Loop.kt
@@ -1,9 +1,17 @@
-// Syntax : 
+// // Syntax : 
+
+// fun main() {
+//     var x = 0 // starting variable
+//     while(x<5) {    // means condition is true till
+//         println("Value of X : $x")
+//         x++
+//     }
+// }
+
+// while true loop : 
 
 fun main() {
-    var x = 0 // starting variable
-    while(x<5) {    // means condition is true till
-        println("Value of X : $x")
-        x++
+    while(true) {
+        println("Always True") // infinite Loop to stop use Ctrl + C
     }
 }
