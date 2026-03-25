f2fc1327bc2587fb0dcf609c1bf07627021d55b6
deepakdotdevs <deepak.06934@gmail.com>
2026-03-25 23:10:29 +0530

    do while (true)

--- If - Else - When - Loops/do_While.kt
+++ If - Else - When - Loops/do_While.kt
@@ -1,10 +1,21 @@
-// do while -> Execute at least one time, even the condition is wrong
+// // do while -> Execute at least one time, even the condition is wrong
+
+// fun main() {
+//     var x = 0;
+//     do{
+//         x++
+//         println(x)
+        
+//     } while(x>5) // even, if (x>5) print one time -> output is 1 
+// }
+
+
+// do while true :
 
 fun main() {
-    var x = 0;
+    var x=0
     do{
         x++
         println(x)
-        
-    } while(x>5) // even, if (x>5) print one time -> output is 1 
+    } while(true)
 }
