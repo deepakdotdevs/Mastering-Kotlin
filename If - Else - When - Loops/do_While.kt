fa88c7a05820e23445239b19022c29189af0372b
deepakdotdevs <deepak.06934@gmail.com>
2026-03-25 22:56:26 +0530

    Do-While

---
+++ If - Else - When - Loops/do_While.kt
@@ -0,0 +1,10 @@
+// do while -> Execute at least one time, even the condition is wrong
+
+fun main() {
+    var x = 0;
+    do{
+        x++
+        println(x)
+        
+    } while(x>5) // even, if (x>5) print one time -> output is 1 
+}
