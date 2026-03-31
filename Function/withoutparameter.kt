f617977db7aba1b526088fae62dcce0a1ec46552
deepakdotdevs <deepak.06934@gmail.com>
2026-03-31 13:19:18 +0530

    Function Intro + sample of creating  function

---
+++ Function/withoutparameter.kt
@@ -0,0 +1,12 @@
+// Function -> create with the fun keyword and it is a block of code that perform a specific task and return a value
+// We have to call it in the main function to execute it
+
+// i) Create a function and call it in the main function with main function print extra value
+
+fun main() {
+    desp(); // call the desp() function
+    println("After the desp() function")
+}
+fun desp() { // creating a function with name desp
+    println("Hello World !!") // body of the function
+}
