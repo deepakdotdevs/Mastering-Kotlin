c7c3a0b7a118ac27a687460777a2c3c3ce937242
deepakdotdevs <deepak.06934@gmail.com>
2026-03-31 13:28:53 +0530

    Function return type #without parameter

--- Function/withoutparameter.kt
+++ Function/withoutparameter.kt
@@ -3,10 +3,32 @@
 
 // i) Create a function and call it in the main function with main function print extra value
 
-fun main() {
-    desp(); // call the desp() function
-    println("After the desp() function")
-}
-fun desp() { // creating a function with name desp
-    println("Hello World !!") // body of the function
-}
+// fun main() {
+//     desp(); // call the desp() function, We call multiple times one function 
+//     println("After the desp() function")
+// }
+// fun desp() { // creating a function with name desp
+//     println("Hello World !!") // body of the function
+// }
+
+// ii) Define return type in a function 
+
+// fun main() {
+//     val total = add()
+//     println(total)
+// }
+// fun add(): Int { // return type is Int, same for String, Foat, Boolean etc
+//     val a = 10
+//     val b = 20
+//     return (a+b)
+// }
+
+// fun main() {
+//     val total = multiply()
+//     println(total)
+// }
+// fun multiply(): String {
+//     val a = 2;
+//     val b = 5
+//     return ("Multiplication - ${a*b}")
+// }
