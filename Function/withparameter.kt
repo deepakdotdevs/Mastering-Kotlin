97b45904f30dca5168546133492dfd7f7521ac69
deepakdotdevs <deepak.06934@gmail.com>
2026-04-03 14:46:10 +0530

    Parameter, Data Type, it's calling with i) example

---
+++ Function/withparameter.kt
@@ -0,0 +1,11 @@
+// parameter under paranthesis
+
+//i) parameter, data type and it's calling
+
+fun main() {
+    disp(10, 20)
+}
+
+fun disp(x: Int, y:Int) {
+    println("Values are the $x and $y")
+}
