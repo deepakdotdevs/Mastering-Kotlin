3286aa0f0ab4c718291b6b41424ddc90ed549933
deepakdotdevs <deepak.06934@gmail.com>
2026-04-04 10:58:27 +0530

    Null Safety #Introduction

---
+++ Null Safety/null_1.kt
@@ -0,0 +1,11 @@
+// i)
+
+// fun main() {
+//     var name:String = "Deepak" // these types of variables cannot be null 
+//     // name = null; // gives error, not allowed 
+
+//     // even if in a condition we must to do it null, then we use ? 
+//     var name2:String? = "Deepak"
+//     name2 = null // allowed
+// }
+
