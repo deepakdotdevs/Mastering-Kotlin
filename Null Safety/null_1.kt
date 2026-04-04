86ce1b0fb16f97fa10ddcca90ee38d29819ca3c5
deepakdotdevs <deepak.06934@gmail.com>
2026-04-04 14:19:27 +0530

    Null Safety #3 Options or Ways of Null Safety

--- Null Safety/null_1.kt
+++ Null Safety/null_1.kt
@@ -9,3 +9,36 @@
 //     name2 = null // allowed
 // }
 
+// ii)
+
+fun main() {
+    var name1: String = "Deepak Jangid"
+    val name1length = name1.length
+    println(name1length)
+
+    // var name2:String? = "Deepak Jangid"
+    // name2 = null
+    // val name2length = name2.length
+    // println(name2length) // name2 is null completely, it gives us error
+    // // but we must need to print it's output, there the 2 options to do it
+
+    // Option 1 : We have to check null condition (Check Null in Conditions)
+    // var name2:String? = "Deepak Jangid"
+    // name2 = null
+
+    // val name2length = if(name2 != null) name2.length else -1
+    // println(name2length)
+
+    // Option 2 : Safe Call (make it using the ?)
+    val name3:String? = "Sahdev Sharma"
+    val name3Length = name3?.length // ? means safe call means -> if not null then gives it's length, if it is null then gives null as it's length but give something atleast
+    println(name3Length)
+
+    // Option 3 : The !! Operator
+    // known as "not null assertion operator (!!)" converts any value to a not null type and throws an exception if the value is null
+
+    var name4:String? = "Vaishnavi"
+     // name4 = null // throw exception
+    val name4Length = name4!!.length
+    println(name4Length)
+}
