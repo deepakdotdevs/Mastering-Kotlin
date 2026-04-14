c8978d9987015b76ae970d6771c313c9d981c09f
deepakdotdevs <deepak.06934@gmail.com>
2026-04-14 20:32:38 +0530

    Private, Protected, Public and Internal #Modifiers

--- Overriding Properties & Functions/visibility_Modifiers.kt
+++ Overriding Properties & Functions/visibility_Modifiers.kt
@@ -9,23 +9,71 @@ adeclaring class sees its internal members.
 
 // All are Public in this case
 
+// open class Father6 {
+//     // properties
+//     var a:Int = 10
+//     var b:Int = 20
+//     var c:Int = 30
+//     var d:Int = 40
+
+//     // member function
+//     fun disp() {
+//         println("A: $a")
+//         println("B: $b")
+//         println("C: $c")
+//         println("D: $d")
+//     }
+
+//     // other member function
+//     fun hello() {
+//         println("Hello Father")
+//     }
+// }
+// class Son6 : Father6() {
+//     // property
+//     var bike:String = "Hunter 350"
+
+//     // member function 
+//     fun show() {
+//         println("A: $a")
+//         println("B: $b")
+//         println("C: $c")
+//         println("D: $d")
+//         hello()
+//     }
+// }
+// fun main() {
+//     val s1 = Son6()
+//     s1.show()
+//     s1.disp()
+//     s1.a = 101
+//     s1.b = 201
+//     s1.c = 301
+//     s1.d = 401
+//     s1.show()
+//     s1.hello()
+
+// }
+
+// Private Case : 
+
 open class Father6 {
     // properties
-    var a:Int = 10
-    var b:Int = 20
-    var c:Int = 30
-    var d:Int = 40
+    private a:Int = 10 // means it can accessed only in this class 
+    protected var b:Int = 20 // protected modifier, can be visible in subclasses
+    internal var c:Int = 30 // internal modifier, can be visible in the same module, and parent class and subclasses also
+    var d:Int = 40 // by default it's public 
 
     // member function
     fun disp() {
-        println("A: $a")
+        println("A: $a") // only use in this
         println("B: $b")
         println("C: $c")
         println("D: $d")
     }
 
     // other member function
-    fun hello() {
+    private fun hello() { // also can be private, protected
         println("Hello Father")
     }
 }
@@ -35,7 +83,7 @@ class Son6 : Father6() {
 
     // member function 
     fun show() {
-        println("A: $a")
+        // println("A: $a") // here it is not workable coz of private modifier, till in inheritance we can't use it
         println("B: $b")
         println("C: $c")
         println("D: $d")
@@ -47,10 +95,16 @@ fun main() {
     s1.show()
     s1.disp()
     s1.a = 101
-    s1.b = 201
+    // s1.b = 201 // can't be accessed coz of protected, only in subclasses and parent class can accessed it
     s1.c = 301
     s1.d = 401
     s1.show()
     s1.hello()
 
+}
+
+// in constructor where we use visibility modifiers, just an example
+
+class Reused private constructor(a:Int) {
+    // body
 }
